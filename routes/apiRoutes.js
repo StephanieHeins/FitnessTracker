e1613adb387dd9b8b56f91eec620fd71913e99ba
Steph Heins <stephanie_heins@yahoo.com>
2021-11-14 00:11:30 -0800

    Added to apiRoutes.js -- Submit new workout, and Update by ID&Body

---
+++ routes/apiRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Workout = require("../models");
+
+// Submit new workout
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  // Update by ID & body 
+  router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id }, { exercises: req.body }
+    ).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.status(400).json(err);
+    });
+});
