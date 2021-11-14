f8899b5523d70aadc4e0a95b536074e00f5bd6d6
Steph Heins <stephanie_heins@yahoo.com>
2021-11-14 00:38:18 -0800

    Added routes to server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // Routes
-require("./routes/apiroutes")(app);
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
